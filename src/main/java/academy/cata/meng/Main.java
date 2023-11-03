ba8cdc926d4c040742c5f5bb0130c09713e2443f
Dmitry Meng <meng.d@soft-logic.ru>
2023-11-03 08:48:16 +0700

    Initial commit

---
+++ src/main/java/academy/cata/meng/Main.java
@@ -0,0 +1,11 @@
+package academy.cata.meng;
+
+/**
+ * @author Dmitry Meng
+ * @since $PROJECT_VERSION
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
