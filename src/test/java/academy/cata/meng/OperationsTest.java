65978dc1bcd1a0526d6f7579029b8b3213d46bbe
Dmitry Meng <meng.d@soft-logic.ru>
2023-11-03 09:01:54 +0700

    declare tests arabic digits

---
+++ src/test/java/academy/cata/meng/OperationsTest.java
@@ -0,0 +1,35 @@
+package academy.cata.meng;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+/**
+ * @author Dmitry Meng
+ * @since 1.0
+ */
+class OperationsTest {
+    @Test
+    void sum_arabic_digits() {
+        assertNull("");
+        //TODO add code
+    }
+
+    @Test
+    void div_arabic_digits() {
+        assertNull("");
+        //TODO add code
+    }
+
+    @Test
+    void muv_arabic_digits() {
+        assertNull("");
+        //TODO add code
+    }
+
+    @Test
+    void sub_arabic_digits() {
+        assertNull("");
+        //TODO add code
+    }
+}
